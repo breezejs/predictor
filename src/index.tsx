37a3f0884d292e3254081d9a7dc7755d213d6f26
Mo Gusbi <mo@Mos-MacBook-Pro.local>
2019-02-09 12:34:14 +0000

    Remove SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap-reboot.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
